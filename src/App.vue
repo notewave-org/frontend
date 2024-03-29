8e0395f57437ffd19006de16652096795745b4c3
Vladimir Galynsky <vdgalyns@yandex.ru>
2024-03-29 21:12:02 +0300

    Layout added

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
-  App
+  <div :class="classes.container">
+    <Sidebar />
+
+    <div :class="classes.content">
+      Текст
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Sidebar from './components/Sidebar.vue'
+</script>
+
+<style module="classes">
+.container {
+  display: grid;
+  grid-template-columns: 20% 1fr;
+  height: 100vh;
+}
+.content {
+  padding: 16px;
+  overflow-y: auto;
+}
+</style>
